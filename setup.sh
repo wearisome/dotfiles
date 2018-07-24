15340bc11a806d0b4b1a9ac93c6027043241f0e1
wearisome <tomita.ryosuke1986@gmail.com>
2018-07-24 14:41:11 +0900

    setup.sh fix'

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-DOT_FILES=()
+DOT_FILES=(.gvimrc)
 
 for file in ${DOT_FILES[@]}
 do
 	ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
-if [ ! -e $HOME/AppData/Local/nvim/.cache/dein ]; then
-		git clone https://github.com/Shougo/dein.vim $HOME/.config/nvim/.cache/dein/repos/github.com/Shougo/dein.vim
+if [ ! -e $HOME/.cache/dein/repos/github.com/Shougo/dein.vim ]; then
+	git clone https://github.com/Shougo/dein.vim $HOME/.cache/dein/repos/github.com/Shougo/dein.vim
 fi
-
-ln -s $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
+if [ ! -e $HOME/.cache/dein/repos/github.com/Shougo/deoplete.vim ]; then
+	git clone https://github.com/Shougo/dein.vim $HOME/.cache/dein/repos/github.com/Shougo/deoplete.vim
+fi
 
