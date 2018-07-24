7482b4c35f56dfd1e43aa5b0e8feee0d48fb4c79
wearisome <tomita.ryosuke1986@gmail.com>
2018-07-24 14:19:28 +0900

    delete init.vim && add .gvimrc

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-DOT_FILES=(.gvimrc)
+DOT_FILES=()
 
 for file in ${DOT_FILES[@]}
 do
 	ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
-ln -s $HOME/dotfiles/init.vim $HOME/AppData/Local/nvim/init.vim
+if [ ! -e $HOME/AppData/Local/nvim/.cache/dein ]; then
+		git clone https://github.com/Shougo/dein.vim $HOME/.config/nvim/.cache/dein/repos/github.com/Shougo/dein.vim
+fi
+
+ln -s $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
 
